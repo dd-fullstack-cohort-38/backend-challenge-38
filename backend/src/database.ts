35a776c4bd7d855c3e737d086d6712555c961fca
George Kephart <gkephart@cnm.edu>
2022-05-11 15:21:34 -0600

    added the new database.ts file version

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,17 +1,17 @@
-import { createPool, Pool } from 'mysql2/promise'
-
-require('dotenv').config()
-
-// Database connection. Can use config from .env file (based on .env) or the defaults
-
-export async function connect (): Promise<Pool> {
-  const connection = await createPool({
+import { Pool, createPool, PoolConnection } from 'mysql2/promise'
+let globalPool: Pool | undefined
+export async function connect (): Promise<PoolConnection> {
+  if (globalPool != null) {
+    return await globalPool.getConnection()
+  }
+  globalPool = await createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     connectionLimit: 10,
+    waitForConnections: true,
     namedPlaceholders: true
   })
-  return connection
+  return await globalPool.getConnection()
 }
